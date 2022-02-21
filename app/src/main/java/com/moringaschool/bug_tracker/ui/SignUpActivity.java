509e57d58c5052daa1aa4c32fc9e4009d59a9a83
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-02-21 07:34:23 +0300

    creating signup UI and adding functionality to the mcreateuserbutton

--- app/src/main/java/com/moringaschool/bug_tracker/ui/SignUpActivity.java
+++ app/src/main/java/com/moringaschool/bug_tracker/ui/SignUpActivity.java
@@ -3,14 +3,38 @@ package com.moringaschool.bug_tracker.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.bug_tracker.R;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.createUserButton) Button mCreateUserButton;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        mCreateUserButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mCreateUserButton){
+            createNewUser();
+        }
     }
 }
